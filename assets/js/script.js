9f2870c05858c0f31ad1f74e06443e477cd96ca8
Ol Hughes <ol@fuzzyblue.monster>
2025-01-29 21:07:01 +0000

    Added division game type

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "divide") {
+        displayDivideQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -85,7 +87,7 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else if (operator === "/") {
+    } else if (operator === "÷") {
         return [operand1 / operand2, "divide"];
     } else {
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -127,5 +129,13 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+
+}
+
+function displayDivideQuestion(operand1, operand2) {
+    
+    document.getElementById("operand1").textContent = operand1 * operand2;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "÷";
 
 }
